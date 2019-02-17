5ed446d976a7bfbb93804f7d1b3692dd78e6e9f0
Yuran Pan <43541124+Yvette-Pan@users.noreply.github.com>
2019-02-17 16:24:18 -0500

    Add files via upload

--- mapreduce-test-master/mapreduce-test-python/logstat_lab1/test_alt.sh
+++ mapreduce-test-master/mapreduce-test-python/logstat_lab1/test_alt.sh
@@ -9,7 +9,7 @@ IFS=- read start end <<< "$range"
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /logstat_lab1/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/access.log /logstat_lab1/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
--file ../../mapreduce-test-python/logstat_lab1/mapper_p2.py -mapper ../../mapreduce-test-python/logstat_lab1/mapper_p2.py \
+-file ../../mapreduce-test-python/logstat_lab1/mapper_p2_alt.py -mapper ../../mapreduce-test-python/logstat_lab1/mapper_p2_alt.py \
 -file ../../mapreduce-test-python/logstat_lab1/reducer_p2.py -reducer ../../mapreduce-test-python/logstat_lab1/reducer_p2.py \
 -input /logstat_lab1/input/* -output /logstat_lab1/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /logstat_lab1/output/part-00000
