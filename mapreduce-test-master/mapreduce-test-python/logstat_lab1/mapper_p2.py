28b29f86e9e6231ce054d882230ec03a82a65fe4
Yuran Pan <43541124+Yvette-Pan@users.noreply.github.com>
2019-02-17 19:56:22 -0500

    Update mapper_p2.py

--- mapreduce-test-master/mapreduce-test-python/logstat_lab1/mapper_p2.py
+++ mapreduce-test-master/mapreduce-test-python/logstat_lab1/mapper_p2.py
@@ -9,7 +9,7 @@ start = int(os.environ['param_h0'])
 end = int(os.environ['param_h1'])
 
 
-pat = re.compile('(?P<ip>\d+.\d+.\d+.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
+pat = re.compile('(?P<ip>\d+[.]\d+[.]\d+[.]\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
 
 for line in sys.stdin:
     match = pat.search(line)
